44a7e93b065c5267169d6d1da858a2af0d179e4c
Tom Monks <monksy@gmail.com>
2024-04-09 10:04:57 -0400

    GridServer get_grid and toggle functions

--- lib/bingo/grid_server.ex
+++ lib/bingo/grid_server.ex
@@ -1,6 +1,8 @@
 defmodule Bingo.GridServer do
   use GenServer
 
+  alias Bingo.Grids
+
   @doc """
   Starts the server.
   """
@@ -15,15 +17,39 @@ defmodule Bingo.GridServer do
     GenServer.call(server, :ping)
   end
 
+  @doc """
+  Retrieves the grid
+  """
+  def get_grid(server) do
+    GenServer.call(server, :get_grid)
+  end
+
+  @doc """
+  Toggles a cell in the grid
+  """
+  def toggle(server, row, col) do
+    GenServer.call(server, {:toggle, row, col})
+  end
+
   @impl true
   def init(:ok) do
-    IO.puts("Starting GridServer")
-    {:ok, %{}}
+    grid = Grids.new_grid()
+    {:ok, grid}
   end
 
   @impl true
   def handle_call(:ping, _from, state) do
-    IO.puts("Received ping")
     {:reply, :pong, state}
+  end
+
+  @impl true
+  def handle_call(:get_grid, _from, state) do
+    {:reply, state, state}
+  end
+
+  @impl true
+  def handle_call({:toggle, row, col}, _from, grid) do
+    grid = Grids.toggle_cell(grid, row, col)
+    {:reply, grid, grid}
   end
 end
--- test/bingo/grid_server_test.exs
+++ test/bingo/grid_server_test.exs
@@ -1,5 +1,6 @@
 defmodule Bingo.GridServerTest do
   use ExUnit.Case, async: true
+
   alias Bingo.GridServer
 
   setup do
@@ -9,5 +10,27 @@ defmodule Bingo.GridServerTest do
 
   test "can ping the server", %{server: server} do
     assert GridServer.ping(server) == :pong
+  end
+
+  test "can retrieve a grid", %{server: server} do
+    assert grid = GridServer.get_grid(server)
+    assert length(grid) == 5
+    assert Enum.at(grid, 0) |> length() == 5
+  end
+
+  test "can toggle a grid cell", %{server: server} do
+    grid = GridServer.get_grid(server)
+
+    # initially false
+    assert {cell_text, false} = get_cell(grid, 0, 0)
+
+    assert returned_grid = GridServer.toggle(server, 0, 0)
+
+    assert {^cell_text, true} = get_cell(returned_grid, 0, 0)
+    assert {^cell_text, true} = GridServer.get_grid(server) |> get_cell(0, 0)
+  end
+
+  defp get_cell(grid, row, col) do
+    Enum.at(grid, row) |> Enum.at(col)
   end
 end
